8c7f2847863cfc6d1c66d15ef12c09eaf089ec4e
Sharon Hsu <sharonhsu@Sharons-MacBook-Air.local>
2017-03-23 11:48:52 -0700

    Add/Remove comments and unnecessary spaces

--- server/index.js
+++ server/index.js
@@ -22,25 +22,16 @@ mongo.connect(MONGODB_URI, (err, db) => {
   // We have a connection to the "tweeter" db, starting here.
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
+  // Because it exports a function that expects the `db` as a parameter, we can
+  // require it and pass the `db` parameter immediately:
   const DataHelpers = require("./lib/data-helpers.js")(db);
+
+  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+  // so it can define routes that use it to interact with the data layer.
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
 });
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-
-
-// Mount the tweets routes at the "/tweets" path prefix:
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
