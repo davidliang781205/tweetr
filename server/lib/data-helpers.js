df8de95b7b5789998ba484717f09a203e80145a8
davidliang781205 <davidliang781205@gmail.com>
2017-03-23 02:09:13 +0000

    finished MongoDB get

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,27 +1,29 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // Saves a tweet to `db`
+    // Saves a tweet to `MongoDB`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.collection("tweeter").push(newTweet);
         callback(null, true);
       });
     },
 
-    // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
-    }
+    // Get all tweets in `MongoDB`, sorted by newest first
+    getTweets: function(callback){
 
+                db.collection("tweeter").find().toArray((err, tweets) => {
+                  if (err) {
+                    return callback(err);
+                  }
+                  console.log(tweets);
+                  callback(null, tweets);
+                });
+    }
   };
 }
